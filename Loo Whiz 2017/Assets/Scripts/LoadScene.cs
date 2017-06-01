d268c9555dd524b64e9b57b707016f90d62eebbb
SIDM\144108f <ikarosdaisuke@gmail.com>
2017-06-01 14:56:49 +0800

    Project Start

---
+++ Loo Whiz 2017/Assets/Scripts/LoadScene.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LoadScene : MonoBehaviour {
+    public void LoadSceneNum(int num)
+    {
+        Debug.LogWarning("Can't load scene number: " + num);
+
+        LoadingScreenManager.LoadScene(num);
+    }	
+}

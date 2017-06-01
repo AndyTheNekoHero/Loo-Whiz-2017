40bbc687e68e2d0701584421b589f552ae31968d
SIDM\144108f <ikarosdaisuke@gmail.com>
2017-06-01 19:45:55 +0800

    UI btn

---
+++ Loo Whiz 2017/Assets/Scripts/GameBtn.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GameBtn : MonoBehaviour {
+
+    public void Mop()
+    {
+        //Character Mop if on Mess
+    }
+    public void FillRoll()
+    {
+        //Character FillRoll if on ToiletBowl
+    }
+    public void Sweep()
+    {
+        //Character Sweep if on Mess
+    }
+    public void Wipe()
+    {
+        //Character wipe if on Mess
+    }
+}

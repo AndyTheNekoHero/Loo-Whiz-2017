80c9be0aef05864363bdee7f9d4e91442092f733
SIDM\144108f <ikarosdaisuke@gmail.com>
2017-06-06 14:40:21 +0800

    NPC script update

---
+++ Loo Whiz 2017/Assets/Scripts/Sink.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Sink : MonoBehaviour {
+
+    public bool InUse()
+    {
+        return true;
+    }
+}
